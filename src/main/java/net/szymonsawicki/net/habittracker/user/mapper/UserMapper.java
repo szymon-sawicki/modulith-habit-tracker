4cc6c286e6c95cc48f4c25c0984669b8395e03e5
szymon-sawicki <szymonsawick@gmail.com>
2024-11-23 22:13:06 +0100

    bumps versions of java to 23, spring boot to 3.3.4, modulith 1.3.0. Introduces proper readme.md, fixes few mappers
    documentation/bruno_collection/

--- src/main/java/net/szymonsawicki/net/habittracker/user/mapper/UserMapper.java
+++ src/main/java/net/szymonsawicki/net/habittracker/user/mapper/UserMapper.java
@@ -4,10 +4,13 @@ import java.util.List;
 import net.szymonsawicki.net.habittracker.user.UserDTO;
 import net.szymonsawicki.net.habittracker.user.model.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
+
+  @Mapping(target = "goals", ignore = true)
   UserDTO toDto(UserEntity user);
 
   UserEntity toEntity(UserDTO user);
